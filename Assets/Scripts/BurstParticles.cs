98e16e1b21975e0e491ccf70a77c7314f24daab5
carl_vitasa <carlvitasa@gmail.com>
2018-06-13 00:20:12 -0400

    started new project called Cloud

---
+++ Assets/Scripts/BurstParticles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BurstParticles : MonoBehaviour 
+{
+	private ParticleSystem ps;
+	ParticleSystem.Particle[] _particles;
+	int numParticlesAlive;
+	private Vector3 particleVelocity;
+	void Start()
+	{
+		ps = GetComponent<ParticleSystem>();
+	}
+
+	void Update () 
+	{
+		_particles = new ParticleSystem.Particle[ps.main.maxParticles];
+		numParticlesAlive = ps.GetParticles(_particles);
+
+		for (int i = 0; i < numParticlesAlive; i++) 
+		{
+			if (_particles[i].velocity.y > 0)
+			{
+				Vector3 newVel = new Vector3(_particles[i].velocity.x, 0, _particles[i].velocity.z);
+				_particles[i].velocity = newVel.normalized * (_particles[i].velocity.magnitude);
+			}
+		}
+		ps.SetParticles(_particles, numParticlesAlive);
+	}
+}
---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,50 @@
+﻿/* TODO: 
+ * while holding down space cloud animation charges down 
+ * jump sound fx variations and small + long jump
+ * burst particles for small and large jump
+ * Maya hat model that always lands face up
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour 
+{
+	[SerializeField] private float _movementSpeed = 4;
+	[SerializeField] private float _jumpVelocity = 5;
+	[SerializeField] private float _fallMultiplyer = 1.5f;
+	[SerializeField] private float _lowJumpMultiplyer = 1.0f;
+	[SerializeField] private GameObject _burst;
+	private Rigidbody _rb;
+	void Start () 
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        transform.Translate(
+			moveHorizontal * _movementSpeed * Time.deltaTime,
+			moveVertical * _movementSpeed * Time.deltaTime,
+			0.0f);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			_rb.velocity = Vector2.up * _jumpVelocity;
+			Instantiate(_burst, new Vector3(transform.position.x, 0.1f, transform.position.z), transform.rotation);
+		}
+
+		if (_rb.velocity.y < 0)
+		{
+			_rb.velocity += Vector3.up * Physics.gravity.y * (_fallMultiplyer - 1) * Time.deltaTime;
+		}
+		else if(_rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+		{
+			_rb.velocity += Vector3.up * Physics.gravity.y * (_lowJumpMultiplyer - 1) * Time.deltaTime;
+		}
+	}
+}
