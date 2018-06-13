28b6c1094bb711f5a8a7378feb9319115f2a4547
Carl Vitasa <cvitasa@magicleap.com>
2018-06-13 17:20:28 -0400

    added hat and started logic for hat

---
+++ Assets/Scripts/Hat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hat : MonoBehaviour 
+{
+	private Rigidbody _rb;
+	private float _floatStrength = 1f;
+	void Start () 
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () 
+	{
+		_rb.AddForce(Vector3.up * _floatStrength);
+	}
+}
